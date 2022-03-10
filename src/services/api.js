c17e0e160cd745073fb936a0a8ae4586f2164404
Vinicius Gomes <viniplay0599@gmail.com>
2022-03-10 16:28:12 -0300

    Req 1: concluded

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,20 @@
+// Requisito 01
+const ENDPOINT_CATEGORY = 'https://api.mercadolibre.com/sites/MLB/categories';
+const ENDPOINT_ID = 'https://api.mercadolibre.com/sites/MLB/search?category=';
+
+// https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=$QUERY
+
 export async function getCategories() {
-  // Implemente aqui
-  // Requisito 01
+  const fetchAPI = await fetch(ENDPOINT_CATEGORY);
+  const data = await fetchAPI.json();
+  // console.log(data);
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const fetchAPI = await fetch(`${ENDPOINT_ID}${categoryId}&q=${query}`);
+  const data = await fetchAPI.json();
+  return data;
 }
+
+// getCategories().then((data) => console.log(data));
