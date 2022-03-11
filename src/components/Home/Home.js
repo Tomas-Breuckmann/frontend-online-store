d3fcfa2c38f4ebf10352df1a6c8ca4616d1ba78d
Your Name <you@example.com>
2022-03-11 17:43:32 -0300

    requisito 4: concluded

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Category from '../categories/Category';
 
 class Home extends Component {
   render() {
     return (
-      <nav>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-        <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
-      </nav>
+      <div>
+        <nav>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+          <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
+        </nav>
+        <Category />
+      </div>
     );
   }
 }
--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -1,15 +1,43 @@
-import React, { Component } from '';
+import React, { Component } from 'react';
+import { getCategories } from '../../services/api';
+
+// [ {
+//     "id": "MLB5672",
+//     "name": "Acessórios para Veículos"
+// } ],
+
+// pegar em uma constante o resultado da api
+// salvar est resultado no state
+// renderizar os .names com os key = .id
 
 class Category extends Component {
   constructor() {
     super();
-
+    this.state = {
+      categories: [],
+    };
   }
+
+  componentDidMount = async () => {
+    const categorias = await getCategories();
+    console.log(categorias);
+    this.setState({ categories: categorias });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
-      
+      <section>
+        {categories.map((element) => (
+          <li
+            data-testid="category"
+            key={ element.id }
+          >
+            {element.name}
+          </li>))}
+      </section>
     );
-  } 
+  }
 }
 
 export default Category;
