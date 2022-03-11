d64a3c02e0c8a285370777d8336b567d5cb0e632
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2022-03-11 16:27:11 -0300

    Requisito 03: concluded

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Cart extends Component {
   render() {
     return (
-      <div>Cart</div>
+      <div>
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      </div>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
-      <div>
+      <nav>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
-      </div>
+        <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
+      </nav>
     );
   }
 }
