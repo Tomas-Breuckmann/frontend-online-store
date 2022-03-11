0ea8ad9e21794fdc93d35a04351f3e68d0636f90
Your Name <you@example.com>
2022-03-11 16:57:58 -0300

    Iniciando requisito 4

---
+++ src/components/categories/Category.js
@@ -0,0 +1,15 @@
+import React, { Component } from '';
+
+class Category extends Component {
+  constructor() {
+    super();
+
+  }
+  render() {
+    return (
+      
+    );
+  } 
+}
+
+export default Category;
